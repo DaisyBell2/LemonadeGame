bb8bfa5dd3ed974762777309294475356681923f
DaisyBell <alina_borz@mail.ru>
2024-08-31 17:34:24 +0300

    LG-01 UiTest

---
+++ app/src/androidTest/java/com/daisybell/lemonadegame/ScenarioTest.kt
@@ -0,0 +1,97 @@
+package com.daisybell.lemonadegame
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ScenarioTest {
+
+    @get:Rule
+    val scenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    private lateinit var gamePage: GamePage
+
+    /**
+     * LGTC-01
+     */
+    @Test
+    fun caseNumber1() {
+        gamePage = GamePage(countTaps = 2)
+        gamePage.assertInitialState()
+
+        gamePage.clickScreen()
+        gamePage.assertSqueezeLemonState()
+
+        gamePage.clickScreen()
+        gamePage.assertSqueezeLemonState()
+
+        gamePage.clickScreen()
+        gamePage.assertLemonadeState()
+
+        gamePage.clickScreen()
+        gamePage.assertEmptyGlassState()
+
+        gamePage.clickScreen()
+        gamePage.assertInitialState()
+    }
+
+    /**
+     * LGTC-02
+     */
+    @Test
+    fun caseNumber2() {
+        gamePage = GamePage(countTaps = 4)
+        gamePage.assertInitialState()
+
+        gamePage.clickScreen()
+        gamePage.assertSqueezeLemonState()
+
+        gamePage.clickScreen()
+        gamePage.assertSqueezeLemonState()
+
+        gamePage.clickScreen()
+        gamePage.assertSqueezeLemonState()
+
+        gamePage.clickScreen()
+        gamePage.assertSqueezeLemonState()
+
+        gamePage.clickScreen()
+        gamePage.assertLemonadeState()
+
+        gamePage.clickScreen()
+        gamePage.assertEmptyGlassState()
+
+        gamePage.clickScreen()
+        gamePage.assertInitialState()
+    }
+
+    /**
+     * LGTC-03
+     */
+    @Test
+    fun caseNumber3() {
+        gamePage = GamePage(countTaps = 3)
+        gamePage.assertInitialState()
+
+        gamePage.clickScreen()
+        gamePage.assertSqueezeLemonState()
+
+        gamePage.clickScreen()
+        gamePage.assertSqueezeLemonState()
+
+        gamePage.clickScreen()
+        gamePage.assertSqueezeLemonState()
+
+        gamePage.clickScreen()
+        gamePage.assertLemonadeState()
+
+        gamePage.clickScreen()
+        gamePage.assertEmptyGlassState()
+
+        gamePage.clickScreen()
+        gamePage.assertInitialState()
+    }
+}
